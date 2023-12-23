4fa4ed2a02d587e98aafc7f1d1a1d377c4bb2dcd
Oleksandra K <a.koroliova@gmail.com>
2023-12-23 20:58:38 +0200

    Solve task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,7 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const body = document.querySelector("body");
+const button = document.querySelector("button.change-color");
+const span = document.querySelector("span.color");
 
-//5 завдання - колір у стилях і показаний в інтерфейсі - однакові
+button.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  span.textContent = String(newColor);
+}
